5ba6dd9cc8da3f82432c41717ba41a4b6f6ea321
KwanJunEr <jonastheitguy@gmail.com>
2025-10-13 13:41:22 +0800

    complete edit and delete post

--- crud_beginner_tutorial/app/Http/Controllers/PostController.php
+++ crud_beginner_tutorial/app/Http/Controllers/PostController.php
@@ -8,6 +8,35 @@ use Illuminate\Support\Facades\Auth;
 
 class PostController extends Controller
 {
+    public function deletePost(Post $post){
+         if(auth()->user()->id === $post['user_id']){
+            $post->delete();
+        }
+        return redirect('/');
+    }
+    public function actuallyUpdatePost(Post $post, Request $request){
+        if(auth()->user()->id !== $post['user_id']){
+            return redirect('/');
+        }
+
+        $incomingFields = $request->validate([
+            'title' => 'required',
+            'body' => 'required',
+        ]);
+
+        $incomingFields['title'] = strip_tags($incomingFields['title']);
+        $incomingFields['body'] = strip_tags($incomingFields['body']);
+
+        $post->update($incomingFields);
+        return redirect('/');
+    }
+    public function showEditScreen(Post $post){  
+        //perform database lookup at dynamic \
+        if(auth()->user()->id !== $post['user_id']){
+            return redirect('/');
+        }
+        return view('edit-post', ['post' => $post]);
+    }
     //
     public function createPost(Request $request){
         $incomingFields = $request->validate([
