6945a6c88396f0dd0b6882253c24318d45c8f10b
KwanJunEr <jonastheitguy@gmail.com>
2025-10-15 14:20:32 +0800

    completed controller section

--- udemy_section8_controller/routes/web.php
+++ udemy_section8_controller/routes/web.php
@@ -1,7 +1,25 @@
 <?php
 
+use App\Http\Controllers\BlogController;
+use App\Http\Controllers\HomeController;
+use App\Http\Controllers\SingleActionController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', [HomeController::class, 'index']);
+
+Route::get('/about', [HomeController::class,'showAboutPage']);
+
+Route::get('/single-action', SingleActionController::class);
+
+//Blog
+//Create
+//Update
+//Read
+//Delete
+
+// Route::post('/blog/create', [BlogController::class, 'create'])->name('blog.create');
+// Route::get('/blog/showw', [BlogController::class, 'show'])->name('blog.show');
+// Route::post('/blog/create', [BlogController::class, 'create'])->name('blog.create');
+// Route::post('/blog/create', [BlogController::class, 'create'])->name('blog.create');
+
+Route::resource('/blog', BlogController::class);
